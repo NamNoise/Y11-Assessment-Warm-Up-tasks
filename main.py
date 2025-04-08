1c437a0054f48b5341284f06bb90eb968df460fb
NamNoise <nam.hoang916@education.nsw.gov.au>
2025-04-08 02:44:53 +0000

    Warmup

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 
 # Input a number from the user and print the square of the number in a message
-
-
-
-
+# number = int(input('Enter a number'))
+# number_squared = number ** 2
+# print(number_squared)
 # input a word and blank out all letters that are not vowels with '='
-
-
-
+word = input('Enter a word: ')
+vowels = {'a', 'd', 'i', 'e', 'u'}
+lastword = ""
+for char in word:
+    if vowels in word:
+        lastword += char
+    else:
+        lastword += '='
+print(lastword)
 # Create a list of words - go through the list and sum the total number of characters in all words
 
 
